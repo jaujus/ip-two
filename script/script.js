34fe505e69b9206b215c2e746b17f668938046f2
oscar omondi <oscar@xodusdigitals.com>
2022-03-08 19:58:57 +0300

    started working on script

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function getAkanName () {
+   //assignemnt 
+    let dayOfBirth = document.getElementById('').ariaValueMin;
+    let monthOfBirth = Number(document.getElementById('').value);
+    let yearOfBirth = Number(ocument.getElementById('').value);
+    let genders = document.getElementById('');
+
+}
