53dccfd1ab6f4ad119c73a3bef6eaee18c001da1
oscar omondi <oscar@xodusdigitals.com>
2022-03-09 01:15:44 +0300

    updated updated ux for html

--- script/script.js
+++ script/script.js
@@ -5,4 +5,12 @@ function getAkanName () {
     let yearOfBirth = Number(ocument.getElementById('').value);
     let genders = document.getElementById('');
 
+    //functions for gender generation
+    function getGender () {
+        for (let gender of genders){
+            if (gender.checked){
+                return gender.value;
+            }
+        }
+    }
 }
